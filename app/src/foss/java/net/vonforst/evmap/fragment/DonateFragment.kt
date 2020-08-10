f3b4c8a8ffe5b285eafc454dac0f9a3855f51ea5
johan12345 <johan.forstner@gmail.com>
2020-08-10 20:31:35 +0200

    implement donations for FOSS version (PayPal)

---
+++ app/src/foss/java/net/vonforst/evmap/fragment/DonateFragment.kt
@@ -0,0 +1,37 @@
+package net.vonforst.evmap.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.foss.fragment_donate.*
+import net.vonforst.evmap.MapsActivity
+import net.vonforst.evmap.R
+
+class DonateFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_donate, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val navController = findNavController()
+        toolbar.setupWithNavController(
+            navController,
+            (requireActivity() as MapsActivity).appBarConfiguration
+        )
+
+        btnDonate.setOnClickListener {
+            (activity as? MapsActivity)?.openUrl(getString(R.string.paypal_link))
+        }
+    }
+}

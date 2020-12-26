ev-map/EVMap

commit 2cd9e9d6428713a06d76975fbd759536710280d7

correct display of opening hours description if there are no opening hours

app/src/main/java/net/vonforst/evmap/adapter/DetailsAdapter.kt
@@ -79,8 +79,11 @@ fun buildDetails(
         if (loc.openinghours != null && !loc.openinghours.isEmpty) DetailsAdapter.Detail(
             R.drawable.ic_hours,
             R.string.hours,
-            loc.openinghours.getStatusText(ctx),
-            loc.openinghours.description,
+            if (loc.openinghours.days != null)
+                loc.openinghours.getStatusText(ctx)
+            else
+                loc.openinghours.description ?: "",
+            if (loc.openinghours.days != null) loc.openinghours.description else null,
             hoursDays = loc.openinghours.days
         ) else null,
         if (loc.cost != null) DetailsAdapter.Detail(

commit e22e1521a4e59696b0ae89a7dfca1a86dde204b8

fix display of 24/7 opening hours
(regression introduced in 2cd9e9d6)

app/src/main/java/net/vonforst/evmap/adapter/DetailsAdapter.kt
@@ -79,7 +79,7 @@ fun buildDetails(
         if (loc.openinghours != null && !loc.openinghours.isEmpty) DetailsAdapter.Detail(
             R.drawable.ic_hours,
             R.string.hours,
-            if (loc.openinghours.days != null)
+            if (loc.openinghours.days != null || loc.openinghours.twentyfourSeven)
                 loc.openinghours.getStatusText(ctx)
             else
                 loc.openinghours.description ?: "",
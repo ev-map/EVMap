7188a2aa64186c041e802996b058a5d64902546b
johan12345 <johan.forstner@gmail.com>
2021-08-24 09:50:25 +0200

    add ACRA for crash reporting

--- app/src/main/java/net/vonforst/evmap/EvMapApplication.kt
+++ app/src/main/java/net/vonforst/evmap/EvMapApplication.kt
@@ -4,6 +4,11 @@ import android.app.Application
 import com.facebook.stetho.Stetho
 import net.vonforst.evmap.storage.PreferenceDataSource
 import net.vonforst.evmap.ui.updateNightMode
+import org.acra.config.dialog
+import org.acra.config.limiter
+import org.acra.config.mailSender
+import org.acra.data.StringFormat
+import org.acra.ktx.initAcra
 
 class EvMapApplication : Application() {
     override fun onCreate() {
@@ -11,5 +16,28 @@ class EvMapApplication : Application() {
         updateNightMode(PreferenceDataSource(this))
         Stetho.initializeWithDefaults(this);
         init(applicationContext)
+
+        if (!BuildConfig.DEBUG) {
+            initAcra {
+                buildConfigClass = BuildConfig::class.java
+                reportFormat = StringFormat.KEY_VALUE_LIST
+
+                mailSender {
+                    mailTo = "evmap+crashreport@vonforst.net"
+                }
+
+                dialog {
+                    text = getString(R.string.crash_report_text)
+                    title = getString(R.string.app_name)
+                    commentPrompt = getString(R.string.crash_report_comment_prompt)
+                    resIcon = R.drawable.ic_launcher_foreground
+                    resTheme = R.style.AppTheme
+                }
+
+                limiter {
+                    enabled = true
+                }
+            }
+        }
     }
 }

f6d9c615a05b64a12744b90a76773ff9ae4cb04b
johan12345 <johan.forstner@gmail.com>
2021-11-14 15:40:13 +0100

    AvailabilityDetectorTest: use proper socket type constants

--- app/src/test/java/net/vonforst/evmap/api/availability/AvailabilityDetectorTest.kt
+++ app/src/test/java/net/vonforst/evmap/api/availability/AvailabilityDetectorTest.kt
@@ -8,13 +8,16 @@ class AvailabilityDetectorTest {
     @Test
     fun testMatchChargepointsSingleCorrect() {
         // single charger with 2 22kW chargepoints
-        val chargepoints = listOf(Chargepoint("Typ2", 22.0, 2))
+        val chargepoints = listOf(Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 22.0, 2))
 
         // correct data in NewMotion
         assertEquals(
             mapOf(chargepoints[0] to setOf(0L, 1L)),
             BaseAvailabilityDetector.matchChargepoints(
-                mapOf(0L to (22.0 to "Typ2"), 1L to (22.0 to "Typ2")),
+                mapOf(
+                    0L to (22.0 to Chargepoint.TYPE_2_UNKNOWN),
+                    1L to (22.0 to Chargepoint.TYPE_2_UNKNOWN)
+                ),
                 chargepoints
             )
         )
@@ -23,13 +26,16 @@ class AvailabilityDetectorTest {
     @Test
     fun testMatchChargepointsSingleWrongPower() {
         // single charger with 2 22kW chargepoints
-        val chargepoints = listOf(Chargepoint("Typ2", 22.0, 2))
+        val chargepoints = listOf(Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 22.0, 2))
 
         // wrong power in NewMotion
         assertEquals(
             mapOf(chargepoints[0] to setOf(0L, 1L)),
             BaseAvailabilityDetector.matchChargepoints(
-                mapOf(0L to (27.0 to "Typ2"), 1L to (27.0 to "Typ2")),
+                mapOf(
+                    0L to (27.0 to Chargepoint.TYPE_2_UNKNOWN),
+                    1L to (27.0 to Chargepoint.TYPE_2_UNKNOWN)
+                ),
                 chargepoints
             )
         )
@@ -38,11 +44,15 @@ class AvailabilityDetectorTest {
     @Test(expected = AvailabilityDetectorException::class)
     fun testMatchChargepointsSingleWrong() {
         // single charger with 2 22kW chargepoints
-        val chargepoints = listOf(Chargepoint("Typ2", 22.0, 2))
+        val chargepoints = listOf(Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 22.0, 2))
 
         // non-matching data in NewMotion
         BaseAvailabilityDetector.matchChargepoints(
-            mapOf(0L to (27.0 to "Typ2"), 1L to (27.0 to "Typ2"), 2L to (50.0 to "CCS")),
+            mapOf(
+                0L to (27.0 to Chargepoint.TYPE_2_UNKNOWN),
+                1L to (27.0 to Chargepoint.TYPE_2_UNKNOWN),
+                2L to (50.0 to Chargepoint.CCS_UNKNOWN)
+            ),
             chargepoints
         )
     }
@@ -51,11 +61,11 @@ class AvailabilityDetectorTest {
     fun testMatchChargepointsComplex() {
         // charger with many different connectors
         val chargepoints = listOf(
-            Chargepoint("Typ2", 43.0, 1),
-            Chargepoint("CCS", 50.0, 1),
-            Chargepoint("CHAdeMO", 50.0, 2),
-            Chargepoint("CCS", 160.0, 1),
-            Chargepoint("CCS", 320.0, 2)
+            Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 43.0, 1),
+            Chargepoint(Chargepoint.CCS_UNKNOWN, 50.0, 1),
+            Chargepoint(Chargepoint.CHADEMO, 50.0, 2),
+            Chargepoint(Chargepoint.CCS_UNKNOWN, 160.0, 1),
+            Chargepoint(Chargepoint.CCS_UNKNOWN, 320.0, 2)
         )
 
         // partly wrong power in NewMotion
@@ -70,15 +80,15 @@ class AvailabilityDetectorTest {
             BaseAvailabilityDetector.matchChargepoints(
                 mapOf(
                     // CHAdeMO + CCS HPC
-                    0L to (50.0 to "CHAdeMO"),
-                    1L to (200.0 to "CCS"),
+                    0L to (50.0 to Chargepoint.CHADEMO),
+                    1L to (200.0 to Chargepoint.CCS_UNKNOWN),
                     // dual CCS HPC
-                    2L to (80.0 to "CCS"),
-                    3L to (200.0 to "CCS"),
+                    2L to (80.0 to Chargepoint.CCS_UNKNOWN),
+                    3L to (200.0 to Chargepoint.CCS_UNKNOWN),
                     // 50kW triple charger
-                    4L to (50.0 to "CCS"),
-                    5L to (50.0 to "CHAdeMO"),
-                    6L to (43.0 to "Typ2")
+                    4L to (50.0 to Chargepoint.CCS_UNKNOWN),
+                    5L to (50.0 to Chargepoint.CHADEMO),
+                    6L to (43.0 to Chargepoint.TYPE_2_UNKNOWN)
                 ),
                 chargepoints
             )
@@ -89,15 +99,18 @@ class AvailabilityDetectorTest {
     fun testMatchChargepointsDifferentPower() {
         // single charger with 1 22kW and 1 11kW chargepoint (common when load balancing is applied)
         val chargepoints = listOf(
-            Chargepoint("Typ2", 22.0, 1),
-            Chargepoint("Typ2", 11.0, 1)
+            Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 22.0, 1),
+            Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 11.0, 1)
         )
 
         // both have 27 kW power in NewMotion
         assertEquals(
             mapOf(chargepoints[1] to setOf(0L), chargepoints[0] to setOf(1L)),
             BaseAvailabilityDetector.matchChargepoints(
-                mapOf(0L to (27.0 to "Typ2"), 1L to (27.0 to "Typ2")),
+                mapOf(
+                    0L to (27.0 to Chargepoint.TYPE_2_UNKNOWN),
+                    1L to (27.0 to Chargepoint.TYPE_2_UNKNOWN)
+                ),
                 chargepoints
             )
         )
@@ -107,8 +120,8 @@ class AvailabilityDetectorTest {
     fun testMatchChargepointsDifferentPower2() {
         // two chargers with 1 22kW and 1 11kW chargepoint (common when load balancing is applied)
         val chargepoints = listOf(
-            Chargepoint("Typ2", 22.0, 2),
-            Chargepoint("Typ2", 11.0, 2)
+            Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 22.0, 2),
+            Chargepoint(Chargepoint.TYPE_2_UNKNOWN, 11.0, 2)
         )
 
         // both have 27 kW power in NewMotion
@@ -116,10 +129,10 @@ class AvailabilityDetectorTest {
             mapOf(chargepoints[1] to setOf(0L, 1L), chargepoints[0] to setOf(2L, 3L)),
             BaseAvailabilityDetector.matchChargepoints(
                 mapOf(
-                    0L to (27.0 to "Typ2"),
-                    1L to (27.0 to "Typ2"),
-                    2L to (27.0 to "Typ2"),
-                    3L to (27.0 to "Typ2")
+                    0L to (27.0 to Chargepoint.TYPE_2_UNKNOWN),
+                    1L to (27.0 to Chargepoint.TYPE_2_UNKNOWN),
+                    2L to (27.0 to Chargepoint.TYPE_2_UNKNOWN),
+                    3L to (27.0 to Chargepoint.TYPE_2_UNKNOWN)
                 ),
                 chargepoints
             )

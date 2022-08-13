5d70d8c09ac69ba8100ec89580c6d26612c37b0f
johan12345 <johan.forstner@gmail.com>
2022-08-13 12:54:22 +0200

    replace deprecated override in NavHostFragment

--- app/src/main/java/net/vonforst/evmap/navigation/NavHostFragment.kt
+++ app/src/main/java/net/vonforst/evmap/navigation/NavHostFragment.kt
@@ -1,12 +1,12 @@
 package net.vonforst.evmap.navigation
 
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
 
 class NavHostFragment : NavHostFragment() {
-    override fun onCreateNavController(navController: NavController) {
-        super.onCreateNavController(navController)
-        navController.navigatorProvider.addNavigator(
+    override fun onCreateNavHostController(navHostController: NavHostController) {
+        super.onCreateNavHostController(navHostController)
+        navHostController.navigatorProvider.addNavigator(
             CustomNavigator(
                 requireContext()
             )

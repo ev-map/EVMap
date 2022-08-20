ev-map/EVMap

commit b445be99bba5f81fa9cea2e397d39cd981f5e7d8

remove dependency on unmaintained Lost library
use Android's own Location APIs instead

app/src/main/java/net/vonforst/evmap/fragment/FavoritesFragment.kt
@@ -1,6 +1,9 @@
 package net.vonforst.evmap.fragment
 
+import android.content.Context
 import android.graphics.Canvas
+import android.location.Criteria
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -20,8 +23,6 @@ import com.car2go.maps.model.LatLng
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.transition.MaterialFadeThrough
-import com.mapzen.android.lost.api.LocationServices
-import com.mapzen.android.lost.api.LostApiClient
 import net.vonforst.evmap.MapsActivity
 import net.vonforst.evmap.R
 import net.vonforst.evmap.adapter.DataBindingAdapter
@@ -34,9 +35,9 @@ import net.vonforst.evmap.utils.checkAnyLocationPermission
 import net.vonforst.evmap.viewmodel.FavoritesViewModel
 import net.vonforst.evmap.viewmodel.viewModelFactory
 
-class FavoritesFragment : Fragment(), LostApiClient.ConnectionCallbacks {
+class FavoritesFragment : Fragment() {
     private lateinit var binding: FragmentFavoritesBinding
-    private var locationClient: LostApiClient? = null
+    private lateinit var locationManager: LocationManager
     private var toDelete: Favorite? = null
     private var deleteSnackbar: Snackbar? = null
     private lateinit var adapter: FavoritesAdapter
@@ -52,8 +53,9 @@ class FavoritesFragment : Fragment(), LostApiClient.ConnectionCallbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        locationClient = LostApiClient.Builder(requireContext())
-            .addConnectionCallbacks(this).build()
+
+        locationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         enterTransition = MaterialFadeThrough()
         exitTransition = MaterialFadeThrough()
@@ -109,8 +111,6 @@ class FavoritesFragment : Fragment(), LostApiClient.ConnectionCallbacks {
         }
         createTouchHelper().attachToRecyclerView(binding.favsList)
 
-        locationClient!!.connect()
-
         binding.swipeRefresh.setOnRefreshListener {
             vm.reloadAvailability() {
                 binding.swipeRefresh.isRefreshing = false
@@ -118,24 +118,18 @@ class FavoritesFragment : Fragment(), LostApiClient.ConnectionCallbacks {
         }
     }
 
-    override fun onConnected() {
-        val context = this.context ?: return
-        if (context.checkAnyLocationPermission()) {
-            val location = LocationServices.FusedLocationApi.getLastLocation(locationClient!!)
-            if (location != null) {
-                vm.location.value = LatLng(location.latitude, location.longitude)
+    override fun onStart() {
+        super.onStart()
+
+        if (requireContext().checkAnyLocationPermission()) {
+            val provider = locationManager.getBestProvider(Criteria().apply {
+                accuracy = Criteria.ACCURACY_FINE
+            }, true) ?: return
+
+            val location = locationManager.getLastKnownLocation(provider)
+            location?.let {
+                vm.location.value = LatLng(it.latitude, it.longitude)
             }
-        }
-    }
-
-    override fun onConnectionSuspended() {
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        locationClient?.let {
-            if (it.isConnected) it.disconnect()
         }
     }
 

commit 41cb6cf6b008a9070e1a562337a7d8e32dba685d

Implement our own new fused location provider for Android API < 31
should address #214
regression since b445be99bba5f81fa9cea2e397d39cd981f5e7d8

app/src/google/java/net/vonforst/evmap/auto/CarAppService.kt
@@ -5,12 +5,9 @@ import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
-import android.location.Criteria
 import android.location.Location
-import android.location.LocationManager
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresPermission
@@ -31,6 +28,9 @@ import androidx.core.location.LocationListenerCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import net.vonforst.evmap.R
+import net.vonforst.evmap.location.FusionEngine
+import net.vonforst.evmap.location.LocationEngine
+import net.vonforst.evmap.location.Priority
 import net.vonforst.evmap.utils.checkFineLocationPermission
 
 
@@ -103,8 +103,8 @@ class EVMapSession(val cas: CarAppService) : Session(), DefaultLifecycleObserver
             location?.let { value?.updateLocation(it) }
         }
     private var location: Location? = null
-    private val locationManager: LocationManager by lazy {
-        carContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    private val locationEngine: LocationEngine by lazy {
+        FusionEngine(carContext)
     }
 
     private val hardwareMan: CarHardwareManager by lazy {
@@ -179,16 +179,11 @@ class EVMapSession(val cas: CarAppService) : Session(), DefaultLifecycleObserver
 
     @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
     private fun requestPhoneLocationUpdates() {
-        val provider = locationManager.getBestProvider(Criteria().apply {
-            accuracy = Criteria.ACCURACY_FINE
-        }, true) ?: return
-
-        val location = locationManager.getLastKnownLocation(provider)
+        val location = locationEngine.getLastKnownLocation()
         updateLocation(location)
-        locationManager.requestLocationUpdates(
-            provider,
+        locationEngine.requestLocationUpdates(
+            Priority.HIGH_ACCURACY,
             1000,
-            1f,
             phoneLocationListener
         )
     }
@@ -209,7 +204,7 @@ class EVMapSession(val cas: CarAppService) : Session(), DefaultLifecycleObserver
 
     @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
     private fun removePhoneLocationUpdates() {
-        locationManager.removeUpdates(phoneLocationListener)
+        locationEngine.removeUpdates(phoneLocationListener)
     }
 
     @SuppressLint("MissingPermission")

app/src/main/java/net/vonforst/evmap/fragment/FavoritesFragment.kt
@@ -1,9 +1,6 @@
 package net.vonforst.evmap.fragment
 
-import android.content.Context
 import android.graphics.Canvas
-import android.location.Criteria
-import android.location.LocationManager
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -29,6 +26,8 @@ import net.vonforst.evmap.adapter.DataBindingAdapter
 import net.vonforst.evmap.adapter.FavoritesAdapter
 import net.vonforst.evmap.databinding.FragmentFavoritesBinding
 import net.vonforst.evmap.databinding.ItemFavoriteBinding
+import net.vonforst.evmap.location.FusionEngine
+import net.vonforst.evmap.location.LocationEngine
 import net.vonforst.evmap.model.Favorite
 import net.vonforst.evmap.model.FavoriteWithDetail
 import net.vonforst.evmap.utils.checkAnyLocationPermission
@@ -37,7 +36,7 @@ import net.vonforst.evmap.viewmodel.viewModelFactory
 
 class FavoritesFragment : Fragment() {
     private lateinit var binding: FragmentFavoritesBinding
-    private lateinit var locationManager: LocationManager
+    private lateinit var locationEngine: LocationEngine
     private var toDelete: Favorite? = null
     private var deleteSnackbar: Snackbar? = null
     private lateinit var adapter: FavoritesAdapter
@@ -54,8 +53,7 @@ class FavoritesFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        locationManager =
-            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationEngine = FusionEngine(requireContext())
 
         enterTransition = MaterialFadeThrough()
         exitTransition = MaterialFadeThrough()
@@ -122,11 +120,7 @@ class FavoritesFragment : Fragment() {
         super.onStart()
 
         if (requireContext().checkAnyLocationPermission()) {
-            val provider = locationManager.getBestProvider(Criteria().apply {
-                accuracy = Criteria.ACCURACY_FINE
-            }, true) ?: return
-
-            val location = locationManager.getLastKnownLocation(provider)
+            val location = locationEngine.getLastKnownLocation()
             location?.let {
                 vm.location.value = LatLng(it.latitude, it.longitude)
             }

app/src/main/java/net/vonforst/evmap/fragment/MapFragment.kt
@@ -6,9 +6,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Configuration
 import android.graphics.Color
-import android.location.Criteria
 import android.location.Geocoder
-import android.location.LocationManager
 import android.os.Bundle
 import android.text.method.KeyListener
 import android.view.*
@@ -79,6 +77,9 @@ import net.vonforst.evmap.autocomplete.ApiUnavailableException
 import net.vonforst.evmap.autocomplete.PlaceWithBounds
 import net.vonforst.evmap.bold
 import net.vonforst.evmap.databinding.FragmentMapBinding
+import net.vonforst.evmap.location.FusionEngine
+import net.vonforst.evmap.location.LocationEngine
+import net.vonforst.evmap.location.Priority
 import net.vonforst.evmap.model.*
 import net.vonforst.evmap.storage.PreferenceDataSource
 import net.vonforst.evmap.ui.*
@@ -100,7 +101,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
     private val galleryVm: GalleryViewModel by activityViewModels()
     private var mapFragment: MapFragment? = null
     private var map: AnyMap? = null
-    private lateinit var locationManager: LocationManager
+    private lateinit var locationEngine: LocationEngine
     private var requestingLocationUpdates = false
     private lateinit var bottomSheetBehavior: BottomSheetBehaviorGoogleMapsLike<View>
     private lateinit var detailAppBarBehavior: MergedAppBarLayoutBehavior
@@ -145,8 +146,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
 
         prefs = PreferenceDataSource(requireContext())
 
-        locationManager =
-            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationEngine = FusionEngine(requireContext())
         clusterIconGenerator = ClusterIconGenerator(requireContext())
 
         enterTransition = MaterialFadeThrough()
@@ -1027,9 +1027,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
 
     @RequiresPermission(anyOf = [ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION])
     private fun moveToLastLocation(map: AnyMap, animate: Boolean) {
-        val provider = getLocationProvider() ?: return
-
-        val location = locationManager.getLastKnownLocation(provider)
+        val location = locationEngine.getLastKnownLocation()
         if (location != null) {
             val latLng = LatLng(location.latitude, location.longitude)
             vm.location.value = latLng
@@ -1041,10 +1039,6 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
             }
         }
     }
-
-    private fun getLocationProvider() = locationManager.getBestProvider(Criteria().apply {
-        accuracy = Criteria.ACCURACY_FINE
-    }, true)
 
     @Synchronized
     private fun updateMap(chargepoints: List<ChargepointListItem>) {
@@ -1300,12 +1294,9 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
 
     @RequiresPermission(anyOf = [ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION])
     private fun requestLocationUpdates() {
-        val provider = getLocationProvider() ?: return
-
-        locationManager.requestLocationUpdates(
-            provider,
+        locationEngine.requestLocationUpdates(
+            Priority.HIGH_ACCURACY,
             5000,
-            1f,
             locationListener
         )
         requestingLocationUpdates = true
@@ -1314,7 +1305,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
     @SuppressLint("MissingPermission")
     private fun removeLocationUpdates() {
         if (context?.checkAnyLocationPermission() == true) {
-            locationManager.removeUpdates(locationListener)
+            locationEngine.removeUpdates(locationListener)
         }
     }
 

app/src/main/java/net/vonforst/evmap/location/FusionEngine.kt
@@ -1,0 +1,262 @@
+package net.vonforst.evmap.location
+
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.os.SystemClock
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.location.LocationListenerCompat
+
+/**
+ * Location engine that fuses GPS and network locations.
+ *
+ * Simplified version of
+ * https://github.com/lostzen/lost/blob/master/lost/src/main/java/com/mapzen/android/lost/internal/FusionEngine.java
+ */
+class FusionEngine(context: Context) : LocationEngine(context),
+    LocationListenerCompat {
+
+    /**
+     * Location updates more than 60 seconds old are considered stale.
+     */
+    private val RECENT_UPDATE_THRESHOLD_IN_MILLIS = (60 * 1000).toLong()
+    private val RECENT_UPDATE_THRESHOLD_IN_NANOS = RECENT_UPDATE_THRESHOLD_IN_MILLIS * 1000000
+    private val TAG = FusionEngine::class.java.simpleName
+
+    private val locationManager =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    private var gpsLocation: Location? = null
+    private var networkLocation: Location? = null
+
+    private val supportsSystemFusedProvider: Boolean
+        get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && locationManager.allProviders.contains(
+            LocationManager.FUSED_PROVIDER
+        )
+
+    override fun getLastKnownLocation(): Location? {
+        if (supportsSystemFusedProvider) {
+            try {
+                return locationManager.getLastKnownLocation(LocationManager.FUSED_PROVIDER)
+            } catch (e: SecurityException) {
+                Log.e(TAG, "Permissions not granted for fused provider", e)
+            }
+        }
+
+        val minTime = SystemClock.elapsedRealtimeNanos() - RECENT_UPDATE_THRESHOLD_IN_NANOS
+        var bestLocation: Location? = null
+        var bestAccuracy = Float.MAX_VALUE
+        var bestTime = Long.MIN_VALUE
+        for (provider in locationManager.allProviders) {
+            try {
+                val location = locationManager.getLastKnownLocation(provider)
+                if (location != null) {
+                    val accuracy = location.accuracy
+                    val time = location.elapsedRealtimeNanos
+                    if (time > minTime && accuracy < bestAccuracy) {
+                        bestLocation = location
+                        bestAccuracy = accuracy
+                        bestTime = time
+                    } else if (time < minTime && bestAccuracy == Float.MAX_VALUE && time > bestTime) {
+                        bestLocation = location
+                        bestTime = time
+                    }
+                }
+            } catch (e: SecurityException) {
+                Log.e(TAG, "Permissions not granted for provider: $provider", e)
+            }
+        }
+        return bestLocation
+    }
+
+    override fun enable() {
+        var networkInterval = Long.MAX_VALUE
+        var gpsInterval = Long.MAX_VALUE
+        var passiveInterval = Long.MAX_VALUE
+        for ((priority, interval) in requests) {
+            when (priority) {
+                Priority.HIGH_ACCURACY -> {
+                    if (interval < gpsInterval) {
+                        gpsInterval = interval
+                    }
+                    if (interval < networkInterval) {
+                        networkInterval = interval
+                    }
+                }
+                Priority.BALANCED_POWER_ACCURACY, Priority.LOW_POWER -> if (interval < networkInterval) {
+                    networkInterval = interval
+                }
+                Priority.NO_POWER -> if (interval < passiveInterval) {
+                    passiveInterval = interval
+                }
+            }
+        }
+
+        if (supportsSystemFusedProvider && gpsInterval < Long.MAX_VALUE) {
+            try {
+                enableFused(gpsInterval)
+                checkLastKnownFused()
+                return
+            } catch (e: SecurityException) {
+                Log.e(TAG, "Permissions not granted for fused provider", e)
+            }
+        }
+
+        var checkGps = false
+        if (gpsInterval < Long.MAX_VALUE) {
+            enableGps(gpsInterval)
+            checkGps = true
+        }
+        if (networkInterval < Long.MAX_VALUE) {
+            enableNetwork(networkInterval)
+            if (checkGps) {
+                val lastGps = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                val lastNetwork =
+                    locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                if (lastGps != null && lastNetwork != null) {
+                    val useGps = lastGps.isBetterThan(lastNetwork)
+                    if (useGps) {
+                        checkLastKnownGps()
+                    } else {
+                        checkLastKnownNetwork()
+                    }
+                } else if (lastGps != null) {
+                    checkLastKnownGps()
+                } else {
+                    checkLastKnownNetwork()
+                }
+            } else {
+                checkLastKnownNetwork()
+            }
+        }
+        if (passiveInterval < Long.MAX_VALUE) {
+            enablePassive(passiveInterval)
+            checkLastKnownPassive()
+        }
+    }
+
+    @Throws(SecurityException::class)
+    override fun disable() {
+        locationManager.removeUpdates(this)
+    }
+
+    @Throws(SecurityException::class)
+    private fun enableGps(interval: Long) {
+        try {
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                interval,
+                0f,
+                this,
+                looper
+            )
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG, "Unable to register for GPS updates.", e)
+        }
+    }
+
+    @Throws(SecurityException::class)
+    private fun enableNetwork(interval: Long) {
+        try {
+            locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                interval,
+                0f,
+                this,
+                looper
+            )
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG, "Unable to register for network updates.", e)
+        }
+    }
+
+    @Throws(SecurityException::class)
+    private fun enablePassive(interval: Long) {
+        try {
+            locationManager.requestLocationUpdates(
+                LocationManager.PASSIVE_PROVIDER,
+                interval,
+                0f,
+                this,
+                looper
+            )
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG, "Unable to register for passive updates.", e)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    @Throws(SecurityException::class)
+    private fun enableFused(interval: Long) {
+        try {
+            locationManager.requestLocationUpdates(
+                LocationManager.FUSED_PROVIDER,
+                interval,
+                0f,
+                this,
+                looper
+            )
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG, "Unable to register for passive updates.", e)
+        }
+    }
+
+    private fun checkLastKnownGps() {
+        checkLastKnownAndNotify(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun checkLastKnownNetwork() {
+        checkLastKnownAndNotify(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkLastKnownPassive() {
+        checkLastKnownAndNotify(LocationManager.PASSIVE_PROVIDER)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun checkLastKnownFused() {
+        checkLastKnownAndNotify(LocationManager.FUSED_PROVIDER)
+    }
+
+    private fun checkLastKnownAndNotify(provider: String) {
+        val location = locationManager.getLastKnownLocation(provider)
+        location?.let { onLocationChanged(it) }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        if (LocationManager.FUSED_PROVIDER == location.provider) {
+            requests.forEach { it.listener.onLocationChanged(location) }
+        } else if (LocationManager.GPS_PROVIDER == location.provider) {
+            gpsLocation = location
+            if (gpsLocation.isBetterThan(networkLocation)) {
+                requests.forEach { it.listener.onLocationChanged(location) }
+            }
+        } else if (LocationManager.NETWORK_PROVIDER == location.provider) {
+            networkLocation = location
+            if (networkLocation.isBetterThan(gpsLocation)) {
+                requests.forEach { it.listener.onLocationChanged(location) }
+            }
+        }
+    }
+
+    private fun Location?.isBetterThan(other: Location?): Boolean {
+        if (this == null) {
+            return false
+        }
+        if (other == null) {
+            return true
+        }
+        if (this.elapsedRealtimeNanos
+            > other.elapsedRealtimeNanos + RECENT_UPDATE_THRESHOLD_IN_NANOS
+        ) {
+            return true
+        }
+        if (!this.hasAccuracy()) {
+            return false
+        }
+        return if (!other.hasAccuracy()) {
+            true
+        } else this.accuracy < other.accuracy
+    }
+}

app/src/main/java/net/vonforst/evmap/location/LocationEngine.kt
@@ -1,0 +1,75 @@
+package net.vonforst.evmap.location
+
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.os.Looper
+
+/**
+ * Base class for [com.mapzen.android.lost.internal.FusionEngine].
+ */
+abstract class LocationEngine(protected val context: Context) {
+    protected val requests: MutableList<LocationRequest> = mutableListOf()
+
+    /**
+     * Return most best recent location available.
+     */
+    abstract fun getLastKnownLocation(): Location?
+
+    /**
+     * Enables the engine on receiving a valid location request.
+     *
+     * @param request Valid location request to enable.
+     */
+    fun requestLocationUpdates(priority: Priority, intervalMs: Long, listener: LocationListener) {
+        requests.add(LocationRequest(priority, intervalMs, listener))
+        enable()
+    }
+
+    /**
+     * Disables the engine when no requests remain, otherwise updates the engine's configuration.
+     *
+     * @param requests Valid location request to enable.
+     */
+    fun removeUpdates(listener: LocationListener) {
+        this.requests.removeIf { it.listener == listener }
+        disable()
+        if (this.requests.isNotEmpty()) enable()
+    }
+
+    fun removeAllRequests() {
+        requests.clear()
+        disable()
+    }
+
+    /**
+     * Subclass should perform all operations required to enable the engine. (ex. Register for
+     * location updates.)
+     */
+    protected abstract fun enable()
+
+    /**
+     * Subclass should perform all operations required to disable the engine. (ex. Remove location
+     * updates.)
+     */
+    protected abstract fun disable()
+    protected val looper: Looper
+        get() = context.mainLooper
+
+    interface Callback {
+        fun reportLocation(location: Location)
+    }
+}
+
+data class LocationRequest(
+    val priority: Priority,
+    val intervalMs: Long,
+    val listener: LocationListener
+)
+
+enum class Priority {
+    HIGH_ACCURACY,
+    BALANCED_POWER_ACCURACY,
+    LOW_POWER,
+    NO_POWER
+}
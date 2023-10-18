cce7c69d745d593b20286cbf3669883ae3a1aea6
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2023-10-18 12:54:24 +0200

    Migrate to build kotlin script (#298)
    * Migrate to build KTS
    
    * fix version code
    
    * fix API keys
    
    * fix kotlinVersion <-> kotlin_version
    
    * fix supportedLocales
    
    * Migrate to build KTS
    
    * upgrade to Gradle 8.4
    
    * updates
    
    * variable renaming
    
    * fix packagingOptions
    
    ---------
    
    Co-authored-by: Johan von Forstner <johan.forstner@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    val kotlinVersion by extra("1.9.0")
+    val aboutLibsVersion by extra("8.9.4")
+    val navVersion by extra("2.7.2")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:$aboutLibsVersion")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        classpath("pt.jcosta.resourceplaceholders:plugin:0.7")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        //noinspection JcenterRepositoryObsolete
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}

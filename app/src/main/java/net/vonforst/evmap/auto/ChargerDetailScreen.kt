6c6e107c6004efea7a69dd2c16789e924e546054
johan12345 <johan.forstner@gmail.com>
2023-11-12 15:14:54 +0100

    OSM: adjustments for AA/AAOS app

--- app/src/main/java/net/vonforst/evmap/auto/ChargerDetailScreen.kt
+++ app/src/main/java/net/vonforst/evmap/auto/ChargerDetailScreen.kt
@@ -60,6 +60,7 @@ import net.vonforst.evmap.storage.ChargeLocationsRepository
 import net.vonforst.evmap.storage.PreferenceDataSource
 import net.vonforst.evmap.ui.ChargerIconGenerator
 import net.vonforst.evmap.ui.getMarkerTint
+import net.vonforst.evmap.utils.formatDMS
 import net.vonforst.evmap.viewmodel.Status
 import net.vonforst.evmap.viewmodel.awaitFinished
 import java.time.ZoneId
@@ -245,7 +246,7 @@ class ChargerDetailScreen(ctx: CarContext, val chargerSparse: ChargeLocation) : 
 
         // Row 1: address + chargepoints
         rows.add(Row.Builder().apply {
-            setTitle(charger.address.toString())
+            setTitle(charger.address?.toString() ?: charger.coordinates.formatDMS())
 
             if (photo == null) {
                 // show just the icon

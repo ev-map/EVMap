26de099baa5d31ab5bd42e2853b8a76539f7bc19
johan12345 <johan.forstner@gmail.com>
2023-12-02 23:23:26 +0100

    WIP: clustering in DB

--- app/src/main/java/net/vonforst/evmap/model/ChargersModel.kt
+++ app/src/main/java/net/vonforst/evmap/model/ChargersModel.kt
@@ -3,6 +3,7 @@ package net.vonforst.evmap.model
 import android.content.Context
 import android.os.Parcelable
 import androidx.core.text.HtmlCompat
+import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -356,8 +357,8 @@ abstract class ChargerPhoto(open val id: String) : Parcelable {
 }
 
 data class ChargeLocationCluster(
-    val clusterCount: Int,
-    val coordinates: Coordinate,
+    @ColumnInfo("clusterCount") val clusterCount: Int,
+    @ColumnInfo("coordinates") val coordinates: Coordinate,
     val items: List<ChargeLocation>? = null
 ) : ChargepointListItem()
 

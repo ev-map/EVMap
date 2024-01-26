640f98c90f13b64c64d8e5b507332de985106651
johan12345 <johan.forstner@gmail.com>
2024-01-26 21:54:37 +0100

    Release 1.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 21
         targetSdk = 34
         // NOTE: always increase versionCode by 2 since automotive flavor uses versionCode + 1
-        versionCode = 206
-        versionName = "1.7.1"
+        versionCode = 208
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += supportedLocales.split(",")

ev-map/EVMap

commit 00b26d224f70188719b591100720fb1e178215e7

fix #329: Layer button reappears after screen rotation

app/src/main/java/net/vonforst/evmap/fragment/MapFragment.kt
@@ -110,6 +110,7 @@ import net.vonforst.evmap.shouldUseImperialUnits
 import net.vonforst.evmap.storage.PreferenceDataSource
 import net.vonforst.evmap.ui.ChargerIconGenerator
 import net.vonforst.evmap.ui.ClusterIconGenerator
+import net.vonforst.evmap.ui.HideOnScrollFabBehavior
 import net.vonforst.evmap.ui.MarkerAnimator
 import net.vonforst.evmap.ui.chargerZ
 import net.vonforst.evmap.ui.clusterZ
@@ -729,6 +730,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
             displaySearchResult(place, moveCamera = true)
         }
         vm.layersMenuOpen.observe(viewLifecycleOwner) { open ->
+            HideOnScrollFabBehavior.from(binding.fabLayers).hidden = open
             binding.fabLayers.visibility = if (open) View.INVISIBLE else View.VISIBLE
             binding.layersSheet.visibility = if (open) View.VISIBLE else View.INVISIBLE
             updateBackPressedCallback()

app/src/main/java/net/vonforst/evmap/ui/HideOnScrollFabBehavior.kt
@@ -12,6 +12,13 @@ import com.mahc.custombottomsheetbehavior.BottomSheetBehaviorGoogleMapsLike
 
 class HideOnScrollFabBehavior(context: Context, attrs: AttributeSet) :
     FloatingActionButton.Behavior(context, attrs) {
+    var hidden: Boolean = false
+
+    companion object {
+        fun from(view: View): HideOnScrollFabBehavior {
+            return ((view.layoutParams as CoordinatorLayout.LayoutParams).behavior as HideOnScrollFabBehavior)
+        }
+    }
 
     override fun onStartNestedScroll(
         coordinatorLayout: CoordinatorLayout,
@@ -61,13 +68,13 @@ class HideOnScrollFabBehavior(context: Context, attrs: AttributeSet) :
         child: FloatingActionButton,
         dependency: View
     ): Boolean {
-        val behavior = BottomSheetBehaviorGoogleMapsLike.from<View>(dependency)
+        val behavior = BottomSheetBehaviorGoogleMapsLike.from(dependency)
         when (behavior.state) {
             BottomSheetBehaviorGoogleMapsLike.STATE_SETTLING -> {
 
             }
             BottomSheetBehaviorGoogleMapsLike.STATE_HIDDEN -> {
-                child.show()
+                if (!hidden) child.show()
             }
             else -> {
                 child.hide()
@@ -103,7 +110,7 @@ class HideOnScrollFabBehavior(context: Context, attrs: AttributeSet) :
             child.hide()
         } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
             // User scrolled up and the FAB is currently not visible -> show the FAB
-            child.show()
+            if (!hidden) child.show()
         }
     }
 }

commit 536c884f23492d7a3bc6fb851440d2c193e0f8a3

fix crash introduced by 00b26d224f70188719b591100720fb1e178215e7

app/src/main/java/net/vonforst/evmap/fragment/MapFragment.kt
@@ -730,7 +730,7 @@ class MapFragment : Fragment(), OnMapReadyCallback, MapsActivity.FragmentCallbac
             displaySearchResult(place, moveCamera = true)
         }
         vm.layersMenuOpen.observe(viewLifecycleOwner) { open ->
-            HideOnScrollFabBehavior.from(binding.fabLayers).hidden = open
+            HideOnScrollFabBehavior.from(binding.fabLayers)?.hidden = open
             binding.fabLayers.visibility = if (open) View.INVISIBLE else View.VISIBLE
             binding.layersSheet.visibility = if (open) View.VISIBLE else View.INVISIBLE
             updateBackPressedCallback()

app/src/main/java/net/vonforst/evmap/ui/HideOnScrollFabBehavior.kt
@@ -15,8 +15,8 @@ class HideOnScrollFabBehavior(context: Context, attrs: AttributeSet) :
     var hidden: Boolean = false
 
     companion object {
-        fun from(view: View): HideOnScrollFabBehavior {
-            return ((view.layoutParams as CoordinatorLayout.LayoutParams).behavior as HideOnScrollFabBehavior)
+        fun from(view: View): HideOnScrollFabBehavior? {
+            return ((view.layoutParams as? CoordinatorLayout.LayoutParams)?.behavior as? HideOnScrollFabBehavior)
         }
     }
 
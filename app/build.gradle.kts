6514197920150f9d66f722bddc3aaef08e472985
johan12345 <johan.forstner@gmail.com>
2024-05-29 20:59:16 +0200

    upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -275,25 +275,25 @@ dependencies {
     val testGoogleImplementation by configurations
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.7.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.browser:browser:1.7.0")
+    implementation("androidx.browser:browser:1.8.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("com.github.ev-map:CustomBottomSheetBehavior:e48f73ea7b")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.12.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
     implementation("com.squareup.moshi:moshi-adapters:1.15.0")
     implementation("com.markomilos.jsonapi:jsonapi-retrofit:1.1.0")
@@ -324,7 +324,7 @@ dependencies {
     }
 
     // Google Places
-    googleImplementation("com.google.android.libraries.places:places:3.3.0")
+    googleImplementation("com.google.android.libraries.places:places:3.5.0")
     googleImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
     // Mapbox Geocoding
@@ -335,7 +335,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     // viewmodel library
-    val lifecycle_version = "2.6.2"
+    val lifecycle_version = "2.8.1"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 
@@ -350,7 +350,7 @@ dependencies {
     implementation("com.github.EV-map:android-spatialite:e5495c83ad")  // version with minSdk increased to 21 & FORTIFY_SOURCE enabled
 
     // billing library
-    val billing_version = "6.1.0"
+    val billing_version = "7.0.0"
     googleImplementation("com.android.billingclient:billing:$billing_version")
     googleImplementation("com.android.billingclient:billing-ktx:$billing_version")
 
@@ -367,7 +367,7 @@ dependencies {
 
     // testing
     testImplementation("junit:junit:4.13.2")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     //noinspection GradleDependency
     testImplementation("org.json:json:20080701")
     testImplementation("org.robolectric:robolectric:4.11.1")

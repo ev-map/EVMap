4c5388350ff19c1d11adbfc74bf6bcfdae54f5fe
johan12345 <johan.forstner@gmail.com>
2024-05-29 20:33:32 +0200

    upgrade car app library to stable 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -308,7 +308,7 @@ dependencies {
     implementation("com.github.romandanylyk:PageIndicatorView:b1bad589b5")
 
     // Android Auto
-    val carAppVersion = "1.4.0-rc02"
+    val carAppVersion = "1.4.0"
     implementation("androidx.car.app:app:$carAppVersion")
     normalImplementation("androidx.car.app:app-projected:$carAppVersion")
     automotiveImplementation("androidx.car.app:app-automotive:$carAppVersion")

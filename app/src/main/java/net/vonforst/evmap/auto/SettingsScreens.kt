0b91805a65ade0417412cc169cce4c64c95866ae
Robert Högberg <robert.hogberg@gmail.com>
2024-11-05 23:01:23 +0100

    Basic NOBIL implementation

--- app/src/main/java/net/vonforst/evmap/auto/SettingsScreens.kt
+++ app/src/main/java/net/vonforst/evmap/auto/SettingsScreens.kt
@@ -451,6 +451,7 @@ class ChooseDataSourceScreen(
     val descriptions = when (type) {
         Type.CHARGER_DATA_SOURCE -> listOf(
             carContext.getString(R.string.data_source_goingelectric_desc),
+            carContext.getString(R.string.data_source_nobil_desc),
             carContext.getString(R.string.data_source_openchargemap_desc),
             carContext.getString(R.string.data_source_openstreetmap_desc)
         )

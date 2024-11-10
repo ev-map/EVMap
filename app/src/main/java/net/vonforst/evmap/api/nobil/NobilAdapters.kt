df7d1292b06661b2c720b5fa40a5c03f454ec6ef
Robert Högberg <robert.hogberg@gmail.com>
2024-11-10 23:14:33 +0100

    Throw an exception if a nobil coordinate fails to parse

--- app/src/main/java/net/vonforst/evmap/api/nobil/NobilAdapters.kt
+++ app/src/main/java/net/vonforst/evmap/api/nobil/NobilAdapters.kt
@@ -1,6 +1,7 @@
 package net.vonforst.evmap.api.nobil
 
 import com.squareup.moshi.FromJson
+import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.ToJson
 import net.vonforst.evmap.model.Coordinate
 import java.time.LocalDateTime
@@ -11,14 +12,12 @@ internal class CoordinateAdapter {
     fun fromJson(position: String): Coordinate {
         val pattern = """\((?<lat>\d+(\.\d+)?), *(?<long>-?\d+(\.\d+)?)\)"""
         val match = Regex(pattern).matchEntire(position)
-        if (match != null) {
-            val groups = match.groups
-            val latitude : String = groups["lat"]?.value?: "0.0"
-            val longitude : String = groups["long"]?.value?: "0.0"
-            return Coordinate(latitude.toDouble(), longitude.toDouble())
-        }
+        if (match == null) throw JsonDataException("Unexpected coordinate format: '$position'")
 
-        return Coordinate(0.0, 0.0) // TODO
+        val groups = match.groups
+        val latitude : String = groups["lat"]?.value?: "0.0"
+        val longitude : String = groups["long"]?.value?: "0.0"
+        return Coordinate(latitude.toDouble(), longitude.toDouble())
     }
     @ToJson
     fun toJson(value: Coordinate): String = "(" + value.lat + ", " + value.lng + ")"

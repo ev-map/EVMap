5471ac5073d040187da60fb238ea1f830329d53d
johan12345 <johan.forstner@gmail.com>
2024-11-13 20:31:33 +0100

    Release 1.9.9 (only for Faurecia Aptoide)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 21
         targetSdk = 34
         // NOTE: always increase versionCode by 2 since automotive flavor uses versionCode + 1
-        versionCode = 234
-        versionName = "1.9.8"
+        versionCode = 236
+        versionName = "1.9.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

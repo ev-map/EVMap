0ea0a2454e747df1b6ca84965965bf41868f21a4
johan12345 <johan.forstner@gmail.com>
2025-01-06 12:18:24 +0100

    update car app library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -311,7 +311,7 @@ dependencies {
     implementation("com.github.erfansn:locale-config-x:1.0.1")
 
     // Android Auto
-    val carAppVersion = "1.7.0-beta01"
+    val carAppVersion = "1.7.0-beta03"
     implementation("androidx.car.app:app:$carAppVersion")
     normalImplementation("androidx.car.app:app-projected:$carAppVersion")
     automotiveImplementation("androidx.car.app:app-automotive:$carAppVersion")

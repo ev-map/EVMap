36af713252e0f6fa2ec1bfbf88e771ae73daa960
johan12345 <johan.forstner@gmail.com>
2025-02-07 19:13:15 +0100

    embed referral links as webpage instead of native Android buttons

--- app/src/main/java/net/vonforst/evmap/fragment/DonateFragmentBase.kt
+++ app/src/main/java/net/vonforst/evmap/fragment/DonateFragmentBase.kt
@@ -1,47 +1,26 @@
 package net.vonforst.evmap.fragment
 
+import android.content.Intent
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import net.vonforst.evmap.MapsActivity
 import net.vonforst.evmap.R
 import net.vonforst.evmap.databinding.FragmentDonateReferralBinding
 
 abstract class DonateFragmentBase : Fragment() {
     fun setupReferrals(referrals: FragmentDonateReferralBinding) {
-        referrals.referralTesla.setOnClickListener {
-            (requireActivity() as MapsActivity).openUrl(
-                getString(R.string.tesla_referral_link),
-                requireView()
-            )
-        }
-        referrals.referralJuicify.setOnClickListener {
-            (requireActivity() as MapsActivity).openUrl(
-                getString(R.string.juicify_referral_link),
-                requireView()
-            )
-        }
-        referrals.referralGeldfuereauto.setOnClickListener {
-            (requireActivity() as MapsActivity).openUrl(
-                getString(R.string.geldfuereauto_referral_link),
-                requireView()
-            )
-        }
-        referrals.referralMaingau.setOnClickListener {
-            (requireActivity() as MapsActivity).openUrl(
-                getString(R.string.maingau_referral_link),
-                requireView()
-            )
-        }
-        referrals.referralEwieeinfach.setOnClickListener {
-            (requireActivity() as MapsActivity).openUrl(
-                getString(R.string.ewieeinfach_referral_link),
-                requireView()
-            )
-        }
-        referrals.referralEprimo.setOnClickListener {
-            (requireActivity() as MapsActivity).openUrl(
-                getString(R.string.eprimo_referral_link),
-                requireView()
-            )
+        referrals.referralWebView.loadUrl(getString(R.string.referral_link))
+        referrals.referralWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                Intent(Intent.ACTION_VIEW, request.url).apply {
+                    startActivity(this)
+                }
+                return true
+            }
         }
     }
 }

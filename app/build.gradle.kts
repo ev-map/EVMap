4c68356ae98d7696c54d3e64a25008ec47ad2b00
johan12345 <johan.forstner@gmail.com>
2025-02-18 21:52:14 +0100

    Release 1.9.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 21
         targetSdk = 34
         // NOTE: always increase versionCode by 2 since automotive flavor uses versionCode + 1
-        versionCode = 244
-        versionName = "1.9.12"
+        versionCode = 246
+        versionName = "1.9.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

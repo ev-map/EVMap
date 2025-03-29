ev-map/EVMap

commit 890af2ddef3fa5dae51039c11d5222a8f58263bf

try to better handle situations where map bounds cross the Antimeridian

app/src/main/java/net/vonforst/evmap/viewmodel/MapViewModel.kt
@@ -1,7 +1,6 @@
 package net.vonforst.evmap.viewmodel
 
 import android.app.Application
-import android.graphics.Point
 import android.os.Parcelable
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
@@ -14,7 +13,6 @@ import androidx.lifecycle.map
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
 import com.car2go.maps.AnyMap
-import com.car2go.maps.Projection
 import com.car2go.maps.model.LatLng
 import com.car2go.maps.model.LatLngBounds
 import com.mahc.custombottomsheetbehavior.BottomSheetBehaviorGoogleMapsLike
@@ -50,7 +48,8 @@ import net.vonforst.evmap.storage.FilterProfile
 import net.vonforst.evmap.storage.PreferenceDataSource
 import net.vonforst.evmap.ui.cluster
 import net.vonforst.evmap.utils.distanceBetween
-import kotlin.math.roundToInt
+import net.vonforst.evmap.utils.normalize
+import kotlin.math.cos
 
 @Parcelize
 data class MapPosition(val bounds: LatLngBounds, val zoom: Float) : Parcelable
@@ -76,7 +75,6 @@ class MapViewModel(application: Application, private val state: SavedStateHandle
         prefs
     )
     private val availabilityRepo = AvailabilityRepository(application)
-    var mapProjection: Projection? = null
 
     val apiId = repo.api.map { it.id }
 
@@ -471,14 +469,21 @@ class MapViewModel(application: Application, private val state: SavedStateHandle
      * expands LatLngBounds beyond the viewport (1.5x the width and height)
      */
     private fun extendBounds(bounds: LatLngBounds): LatLngBounds {
-        val mapProjection = mapProjection ?: return bounds
-        val swPoint = mapProjection.toScreenLocation(bounds.southwest)
-        val nePoint = mapProjection.toScreenLocation(bounds.northeast)
-        val dx = ((nePoint.x - swPoint.x) * 0.25).roundToInt()
-        val dy = ((nePoint.y - swPoint.y) * 0.25).roundToInt()
-        val newSw = mapProjection.fromScreenLocation(Point(swPoint.x - dx, swPoint.y - dy))
-        val newNe = mapProjection.fromScreenLocation(Point(nePoint.x + dx, nePoint.y + dy))
-        return LatLngBounds(newSw, newNe)
+        val sw = bounds.southwest
+        val ne = bounds.northeast
+        var west = sw.longitude - (ne.longitude - sw.longitude) * 0.25
+        var east = ne.longitude + (ne.longitude + sw.longitude) * 0.25
+        val south =
+            sw.latitude - (ne.latitude - sw.latitude) * 0.25 * cos(Math.toRadians(sw.latitude))
+        val north =
+            ne.latitude + (ne.latitude - sw.latitude) * 0.25 * cos(Math.toRadians(ne.latitude))
+
+        if (east - west >= 360) {
+            west = -180.0
+            east = 180.0
+        }
+
+        return LatLngBounds(LatLng(south, west), LatLng(north, east)).normalize()
     }
 
     fun reloadAvailability() {

app/src/main/java/net/vonforst/evmap/fragment/MapFragment.kt
@@ -1065,7 +1065,6 @@ class MapFragment : Fragment(), OnMapReadyCallback, MenuProvider {
 
     override fun onMapReady(map: AnyMap) {
         this.map = map
-        vm.mapProjection = map.projection
         val context = this.context ?: return
         view ?: return
 
@@ -1095,14 +1094,12 @@ class MapFragment : Fragment(), OnMapReadyCallback, MenuProvider {
         map.uiSettings.setIndoorLevelPickerEnabled(false)
 
         map.setOnCameraIdleListener {
-            vm.mapProjection = map.projection
             vm.mapPosition.value = MapPosition(
                 map.projection.visibleRegion.latLngBounds, map.cameraPosition.zoom
             )
             vm.reloadChargepoints()
         }
         map.setOnCameraMoveListener {
-            vm.mapProjection = map.projection
             vm.mapPosition.value = MapPosition(
                 map.projection.visibleRegion.latLngBounds, map.cameraPosition.zoom
             )
@@ -1602,12 +1599,10 @@ class MapFragment : Fragment(), OnMapReadyCallback, MenuProvider {
     override fun onDestroyView() {
         super.onDestroyView()
         detailsDialog.onDestroy()
-        vm.mapProjection = null
 
         map = null
         mapFragment = null
         _binding = null
-        vm.mapProjection = null
         markers.clear()
         clusterMarkers = emptyList()
         searchResultMarker = null

app/src/main/java/net/vonforst/evmap/storage/ChargeLocationsDao.kt
@@ -19,6 +19,8 @@ import net.vonforst.evmap.api.goingelectric.GoingElectricApiWrapper
 import net.vonforst.evmap.api.openchargemap.OpenChargeMapApiWrapper
 import net.vonforst.evmap.model.*
 import net.vonforst.evmap.ui.cluster
+import net.vonforst.evmap.utils.crossesAntimeridian
+import net.vonforst.evmap.utils.splitAtAntimeridian
 import net.vonforst.evmap.viewmodel.Resource
 import net.vonforst.evmap.viewmodel.Status
 import net.vonforst.evmap.viewmodel.await
@@ -145,6 +147,14 @@ class ChargeLocationsRepository(
         filters: FilterValues?,
         overrideCache: Boolean = false
     ): LiveData<Resource<List<ChargepointListItem>>> {
+        if (bounds.crossesAntimeridian()) {
+            val (a, b) = bounds.splitAtAntimeridian()
+            val liveDataA = getChargepoints(a, zoom, filters, overrideCache)
+            val liveDataB = getChargepoints(b, zoom, filters, overrideCache)
+            return combineLiveData(liveDataA, liveDataB)
+        }
+        
+        
         val api = api.value!!
 
         val dbResult = if (filters == null) {
@@ -205,6 +215,32 @@ class ChargeLocationsRepository(
             apiResult
         } else {
             CacheLiveData(dbResult, apiResult, savedRegionResult).distinctUntilChanged()
+        }
+    }
+
+    private fun combineLiveData(
+        liveDataA: LiveData<Resource<List<ChargepointListItem>>>,
+        liveDataB: LiveData<Resource<List<ChargepointListItem>>>
+    ) = MediatorLiveData<Resource<List<ChargepointListItem>>>().apply {
+        listOf(liveDataA, liveDataB).forEach {
+            addSource(it) {
+                val valA = liveDataA.value
+                val valB = liveDataB.value
+                val combinedList = if (valA?.data != null && valB?.data != null) {
+                    valA.data + valB.data
+                } else if (valA?.data != null) {
+                    valA.data
+                } else if (valB?.data != null) {
+                    valB.data
+                } else null
+                if (valA?.status == Status.SUCCESS && valB?.status == Status.SUCCESS) {
+                    Resource.success(combinedList)
+                } else if (valA?.status == Status.ERROR || valB?.status == Status.ERROR) {
+                    Resource.error(valA?.message ?: valB?.message, combinedList)
+                } else {
+                    Resource.loading(combinedList)
+                }
+            }
         }
     }
 

app/src/main/java/net/vonforst/evmap/utils/LocationUtils.kt
@@ -9,8 +9,15 @@ import androidx.core.content.ContextCompat
 import com.car2go.maps.model.LatLng
 import com.car2go.maps.model.LatLngBounds
 import net.vonforst.evmap.model.Coordinate
-import java.util.*
-import kotlin.math.*
+import java.util.Locale
+import kotlin.math.abs
+import kotlin.math.asin
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 /**
  * Adds a certain distance in meters to a location. Approximate calculation.
@@ -152,4 +159,25 @@ fun Coordinate.formatDecimal(accuracy: Int = 6): String {
 
 fun Location.formatDecimal(accuracy: Int = 6): String {
     return "%.${accuracy}f, %.${accuracy}f".format(Locale.ENGLISH, latitude, longitude)
+}
+
+fun LatLngBounds.normalize() = LatLngBounds(
+    LatLng(southwest.latitude, normalizeLongitude(southwest.longitude)),
+    LatLng(northeast.latitude, normalizeLongitude(northeast.longitude)),
+)
+
+private fun normalizeLongitude(long: Double) =
+    if (-180.0 <= long && long <= 180.0) long else (long + 180) % 360 - 180
+
+fun LatLngBounds.crossesAntimeridian() = southwest.longitude > 0 && northeast.longitude < 0
+
+fun LatLngBounds.splitAtAntimeridian(): Pair<LatLngBounds, LatLngBounds> {
+    if (!crossesAntimeridian()) throw IllegalArgumentException("does not cross antimeridian")
+    return LatLngBounds(
+        LatLng(southwest.latitude, southwest.longitude),
+        LatLng(northeast.latitude, 180.0),
+    ) to LatLngBounds(
+        LatLng(southwest.latitude, -180.0),
+        LatLng(northeast.latitude, northeast.longitude),
+    )
 }

commit e9b70a2f003f7d399d47b747cd2873f9966ba1ff

fix bug in extendBounds
introduced in 890af2ddef3fa5dae51039c11d5222a8f58263bf
fixes #373

app/src/main/java/net/vonforst/evmap/viewmodel/MapViewModel.kt
@@ -472,7 +472,7 @@ class MapViewModel(application: Application, private val state: SavedStateHandle
         val sw = bounds.southwest
         val ne = bounds.northeast
         var west = sw.longitude - (ne.longitude - sw.longitude) * 0.25
-        var east = ne.longitude + (ne.longitude + sw.longitude) * 0.25
+        var east = ne.longitude + (ne.longitude - sw.longitude) * 0.25
         val south =
             sw.latitude - (ne.latitude - sw.latitude) * 0.25 * cos(Math.toRadians(sw.latitude))
         val north =
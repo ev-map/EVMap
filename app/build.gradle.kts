dcbe4c63256f9452571d7b2465a3ac2d6c25c7a1
johan12345 <johan.forstner@gmail.com>
2025-05-29 00:41:28 +0200

    Release 1.9.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 21
         targetSdk = 34
         // NOTE: always increase versionCode by 2 since automotive flavor uses versionCode + 1
-        versionCode = 252
-        versionName = "1.9.16"
+        versionCode = 254
+        versionName = "1.9.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
